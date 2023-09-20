83eeaf537a38d6d11cbc6dc911e8af87f0f0740a
Tobias Fenner <tfenner@usc.edu>
2023-09-19 17:49:04 -0700

    time to first byte

--- background.js
+++ background.js
@@ -1,69 +1,80 @@
-// Background Script (background.js)
+let ttfbValue = 0;  // Store the TTFB value received from content script
+
+// Listener for messages from the content script
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'ttfb') {
+        ttfbValue = message.value;
+    }
+});
 
 // Define variables to measure page load time
 let pageLoadStartTime;
 
-chrome.webNavigation.onCommitted.addListener(function (details) {
-  // Extract the URL of the committed navigation
-  const url = details.url;
-
-  // Log the URL to the console for debugging
-  console.log('Navigation Committed for URL:', url);
-
-  // Start measuring page load time
-  pageLoadStartTime = performance.now();
+chrome.webNavigation.onCommitted.addListener(function(details) {
+    // Start measuring page load time
+    pageLoadStartTime = performance.now();
 });
 
-chrome.webNavigation.onCompleted.addListener(async function (details) {
-  // Extract the URL of the completed navigation
-  const url = details.url;
+chrome.webNavigation.onCompleted.addListener(async function(details) {
+    // Calculate and log the page load time
+    const pageLoadTime = calculatePageLoadTime();
 
-  // Log the URL to the console for debugging
-  console.log('Navigation Completed for URL:', url);
-
-  // Calculate and log the page load time
-  const pageLoadTime = calculatePageLoadTime();
-  console.log('Page Load Time (ms):', pageLoadTime);
-
-  // Perform HTTP status code check for the current tab's URL
-  checkHttpStatus(url, function (statusCode) {
-    // Send a message to the content script to show the popup with the response code and page load time
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'showPopup', statusCode: statusCode, pageLoadTime: pageLoadTime });
+    // Perform HTTP status code check for the current tab's URL
+    const url = details.url;
+    checkHttpStatus(url, async function(statusCode) {
+        // Send a message to the content script to show the popup with the response code and page load time
+        chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
+            const dnsResponseCode = await getDNSResponseCode();  // Fetching DNS Response Code
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: 'showPopup',
+                statusCode: statusCode,
+                dnsResponseCode: dnsResponseCode,
+                pageLoadTime: pageLoadTime,
+                ttfb: ttfbValue  // Send the stored TTFB value
+            });
+        });
     });
-  });
 });
+
 
 // Function to calculate page load time
 function calculatePageLoadTime() {
-  if (pageLoadStartTime) {
-    const pageLoadEndTime = performance.now();
-    return pageLoadEndTime - pageLoadStartTime;
-  } else {
-    return 'Page load time not available';
-  }
+    if (pageLoadStartTime) {
+        const pageLoadEndTime = performance.now();
+        return pageLoadEndTime - pageLoadStartTime;
+    } else {
+        return 'Page load time not available';
+    }
 }
 
 // Function to check HTTP status code
 async function checkHttpStatus(url, callback) {
-  try {
-    // Log the HTTP status code check attempt for debugging
-    console.log('Checking HTTP Status Code for URL:', url);
-
-    const response = await fetch(url, { method: 'HEAD', mode: 'no-cors' });
-
-    if (response) {
-      const statusCode = response.status;
-
-      // Pass the HTTP status code to the callback
-      callback(statusCode);
-    } else {
-      console.error('HTTP Status Code Check Error: Response is null or undefined');
-      callback(0); // 0 indicates an error
+    try {
+        const response = await fetch(url, { method: 'HEAD', mode: 'no-cors' });
+        if (response) {
+            const statusCode = response.status;
+            callback(statusCode);
+        } else {
+            console.error('HTTP Status Code Check Error: Response is null or undefined');
+            callback(0);  // 0 indicates an error
+        }
+    } catch (error) {
+        console.error('HTTP Status Code Check Error:', error);
+        callback(0);  // 0 indicates an error
     }
-  } catch (error) {
-    // An error occurred while checking the HTTP status code
-    console.error('HTTP Status Code Check Error:', error);
-    callback(0); // 0 indicates an error
-  }
+}
+
+// Function to get DNS Response Code (simulating using a fetch to a known endpoint)
+async function getDNSResponseCode() {
+    try {
+        const response = await fetch('https://example.com', { method: 'HEAD', mode: 'no-cors' });
+        if (response) {
+            return response.status;
+        } else {
+            return 'Failed to fetch DNS response code';
+        }
+    } catch (error) {
+        console.error('DNS Response Code Check Error:', error);
+        return 'Failed to fetch DNS response code';
+    }
 }
--- content.js
+++ content.js
@@ -1,42 +1,45 @@
-// content.js
+// Calculate the TTFB (Time To First Byte)
+function getTTFB() {
+    const performanceTiming = window.performance.timing;
+    return performanceTiming.responseStart - performanceTiming.navigationStart;
+}
 
-chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+// Send the TTFB value to the background script
+const ttfb = getTTFB();
+chrome.runtime.sendMessage({ action: 'ttfb', value: ttfb });
+
+// Listener for messages from the background script
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === 'showPopup') {
-      const statusCode = message.statusCode;
-      const currentTime = getCurrentTime(); // Get the current time
-      const dnsResponseCode = await getDNSResponseCode(); // Get DNS response code
-      const pageLoadTime = message.pageLoadTime; // Get the page load time from the message
-  
-      displayPopup(statusCode, currentTime, dnsResponseCode, pageLoadTime);
+        const statusCode = message.statusCode;
+        const currentTime = getCurrentTime();
+        const dnsResponseCode = message.dnsResponseCode;
+        const pageLoadTime = message.pageLoadTime;
+        const ttfb = message.ttfb;  // TTFB data received from the message
+
+        displayPopup(statusCode, currentTime, dnsResponseCode, pageLoadTime, ttfb);
     }
-  });
-  
-  function getCurrentTime() {
+});
+
+// Returns the current time in HH:MM:SS format
+function getCurrentTime() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
-  }
-  
-  async function getDNSResponseCode() {
-    try {
-      const response = await fetch('https://example.com', { method: 'HEAD', mode: 'no-cors' });
-      if (response) {
-        const dnsResponseCode = response.status;
-        return dnsResponseCode;
-      } else {
-        return 'Failed to fetch DNS response code';
-      }
-    } catch (error) {
-      console.error('DNS Response Code Check Error:', error);
-      return 'Failed to fetch DNS response code';
-    }
-  }
-  
-  function displayPopup(statusCode, currentTime, dnsResponseCode, pageLoadTime) {
+}
+
+// Displays the popup with various performance metrics
+function displayPopup(statusCode, currentTime, dnsResponseCode, pageLoadTime, ttfb) {
     const popupDiv = document.createElement('div');
-    popupDiv.innerHTML = `Current Time: ${currentTime}<br>HTTP Response Code: ${statusCode}<br>DNS Response Code: ${dnsResponseCode}<br>Page Load Time: ${pageLoadTime.toFixed(2)} ms`;
+    popupDiv.innerHTML = `
+        Current Time: ${currentTime}<br>
+        HTTP Response Code: ${statusCode}<br>
+        DNS Response Code: ${dnsResponseCode}<br>
+        Page Load Time: ${pageLoadTime.toFixed(2)} ms<br>
+        TTFB: ${ttfb.toFixed(2)} ms`; // Display the TTFB in the popup
+
     popupDiv.style.position = 'fixed';
     popupDiv.style.zIndex = 9999;
     popupDiv.style.top = '10px';
@@ -46,9 +49,6 @@ chrome.runtime.onMessage.addListener(async function (message, sender, sendRespon
     popupDiv.style.padding = '10px';
     popupDiv.style.fontSize = '14px';
     popupDiv.style.color = 'black';
+    
     document.body.appendChild(popupDiv);
-    // setTimeout(() => {
-    //   popupDiv.remove();
-    // }, 5000);
-  }
-  
+}
