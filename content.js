f8ef79d128911d1dd4dcd14b4333db21dc5e642d
Tobias Fenner <tfenner@usc.edu>
2023-09-28 11:59:43 -0700

    fixed banner

--- content.js
+++ content.js
@@ -37,12 +37,7 @@ function displayPopup(statusCode, currentTime, pageLoadTime, ttfb, errorType, er
 
     bannerDiv.innerHTML = `
         <div style="display: inline-block; white-space: nowrap;">
-            <span>Current Time: ${currentTime}</span> |
-            <span>HTTP Response Code: ${statusCode}</span> |
-            <span>Page Load Time: ${pageLoadTime.toFixed(2)} ms</span> |
-            <span>TTFB: ${ttfb.toFixed(2)} ms</span> |
-            <span>Error Type: ${errorType}</span> |
-            <span>Error Description: ${errorDescription}</span>
+            <span>ERROR: Web page could not be displayed.</span>
         </div>
         <div style="float: right; display: inline-block; outline: none">
         <a href="https://www.google.com" style="background-color: #4285F4; color: white; padding: 8px 20px; margin: 2px 10px 2px 0; text-decoration: none; border-radius: 5px; outline: none;">Go to fixed link</a>
