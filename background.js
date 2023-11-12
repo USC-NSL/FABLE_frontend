66dd5e5217b2e4279934d1f39f637db4567eb0fc
Tobias Fenner <tfenner@usc.edu>
2023-11-12 15:14:00 -0800

    before heuristics

--- background.js
+++ background.js
@@ -1,191 +1,54 @@
-let ttfbValue = 0;  // Store the TTFB value received from content script
+// background.js
 
-let lastMetrics = {
-    statusCode: null,
-    currentTime: null,
-    // dnsResponseCode: null,
-    pageLoadTime: null,
-    ttfb: null,
-    errorType: null,
-    errorDescription: null
-};
+// background.js
 
+chrome.webRequest.onCompleted.addListener(
+    function(details) {
+        if (details.type === "main_frame" && details.frameId === 0 && !details.url.startsWith("chrome://")) {
+            console.log('Main frame load detected for:', details.url);
 
-// Listener for messages from the content script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(`Message received from content script: ${message.action}`);
-    if (message.action === 'ttfb') {
-        console.log(`TTFB value received: ${message.value}`);
-        ttfbValue = message.value;
-    }
-});
+            // Inject the content script
+            if (details.tabId > 0) {
+                chrome.scripting.executeScript({
+                    target: { tabId: details.tabId },
+                    files: ['content.js']
+                }, () => {
+                    if (chrome.runtime.lastError) {
+                        console.error('Script injection failed:', chrome.runtime.lastError.message);
+                        return;
+                    }
 
-// Define variables to measure page load time
-let pageLoadStartTime;
-
-chrome.webNavigation.onCommitted.addListener(function(details) {
-    console.log(`Navigation committed for url: ${details.url}`);
-    if (!details.url.startsWith("chrome://")) {
-        console.log(`Starting page load time measurement for url: ${details.url}`);
-        pageLoadStartTime = performance.now();
-    }
-});
-
-
-
-// Function to get a description of the network error
-function getErrorDescription(errorType) {
-    console.log(`Getting error description for error type: ${errorType}`);
-    switch (errorType) {
-        case 'net::ERR_NAME_NOT_RESOLVED':
-            return 'DNS resolution failure';
-        case 'net::ERR_CONNECTION_TIMED_OUT':
-            return 'Connection timed out';
-        case 'net::ERR_CONNECTION_REFUSED':
-            return 'Connection refused';
-        case 'net::ERR_CONNECTION_RESET':
-            return 'Connection reset';
-        case 'net::ERR_SSL_PROTOCOL_ERROR':
-            return 'SSL protocol error';
-        case 'net::ERR_EMPTY_RESPONSE':
-            return 'Empty response received';
-        case 'net::ERR_BLOCKED_BY_CLIENT':
-            return 'Blocked by client (e.g., ad blocker)';
-        // Add more error types and descriptions as needed
-        default:
-            console.log(`Unknown error type received: ${errorType}`);
-            return 'Unknown error';
-    }
-}
-
-//is this just html or e.g. js
-//no iframe but missing image or css file
-//focusing only on iframes may not be sufficient
-//debug console show on all places onERrorOccured is called 
-
-//webRequest.onErrorOccurred
-// Listener for errors on a per-resource basis
-chrome.webRequest.onErrorOccurred.addListener(
-    function (details) {
-        console.log(`Web request error: ${details.error} for url: ${details.url}`);
-
-        // Prepare the error details
-        const errorType = details.error;
-        const errorDescription = getResourceErrorDescription(errorType);
-
-        // Store the error information in lastMetrics or handle it as needed
-        lastMetrics.errorType = errorType;
-        lastMetrics.errorDescription = errorDescription;
-
-        // Here you can choose to send a message to the content script if needed,
-        // or you could just log the error, or store it for further processing.
-        // Example:
-        if (details.tabId > 0) {
-            chrome.tabs.sendMessage(details.tabId, {
-                action: 'logError',
-                errorType: errorType,
-                errorDescription: errorDescription
-            });
-        }
-
-        // If the error URL matches a specific pattern, you might consider it a soft 404
-        if (details.url && somePatternThatIndicatesSoft404(details.url)) {
-            soft404Urls.push(details.url);
-        }
-
-        // ... your additional logic for handling the error
-    },
-    { urls: ["<all_urls>"] } // Use appropriate filters as needed
-);
-
-// Helper function to determine soft 404 (this is a simple example, replace with your actual logic)
-function somePatternThatIndicatesSoft404(url) {
-    // Example: a URL that does not end with a file extension might be a dynamic page that could soft 404
-    return !url.match(/\.\w+($|\?)/);
-}
-
-chrome.webNavigation.onErrorOccurred.addListener(function(details) {
-    console.log(`Error occurred for url: ${details.url} with error: ${details.error}`);
-    if (details.frameId === 0 && !details.url.startsWith("chrome://")) {
-        const errorType = details.error;
-        const errorDescription = getErrorDescription(errorType);
-        console.log(`Sending error message to content script for url: ${details.url}`);
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: 'showPopup',
-                errorType: errorType,
-                errorDescription: errorDescription,
-                ttfb: ttfbValue,
-                pageLoadTime: "N/A" // Since it's a network error, page load time might not be applicable
-            });
-        });
-    }
-});
-
-// This function will return a custom error description
-function getResourceErrorDescription(error) {
-    switch (error) {
-        case 'net::ERR_ABORTED':
-            return 'The request was aborted.';
-        case 'net::ERR_FAILED':
-            return 'The request failed.';
-        // ... (include other specific cases you want to handle)
-        default:
-            return 'An unknown error occurred.';
-    }
-}
-
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    // Check if it's the main frame and not an internal Chrome page
-    console.log('Navigation completed for:', details.url, 'in tab:', details.tabId, 'frame:', details.frameId);
-
-    if (details.frameId === 0 && !details.url.startsWith("chrome://")) {
-        const url = details.url;
-        checkHttpStatus(url, function(statusCode) {
-            if (statusCode >= 400) {
-                chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, {
-                        action: 'showPopup',
-                        statusCode: statusCode,
-                        pageLoadTime: calculatePageLoadTime(),
-                        ttfb: ttfbValue,
-                        errorType: "(HTTP Error)",
-                        errorDescription: `HTTP ${statusCode} Error` 
-                    });
+                    // Check if the request returned a 404 status
+                    if ((details.statusCode >= 400 && details.statusCode < 500) || (details.statusCode >= 500 && details.statusCode < 600)) {
+                        console.log('Detected a 4xx or 5xx error for main frame:', details.url);
+                        chrome.tabs.sendMessage(details.tabId, { action: 'displayPopup' });
+                    } else {
+                        // If status code is not in the 400s or 500s, check the title
+                        chrome.tabs.sendMessage(details.tabId, { action: 'checkTitleFor404' });
+                        chrome.tabs.sendMessage(details.tabId, { action: 'checkForSparseContent' });
+                    }
                 });
             }
-        });
+        }
+    },
+    { urls: ["<all_urls>"] },
+    ["responseHeaders"]
+);
+
+// Listener in background.js to receive messages from content.js
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'displayPopupForTitle404' && sender.tab) {
+        chrome.tabs.sendMessage(sender.tab.id, { action: 'displayPopup' });
     }
 });
 
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(`Message received for action: ${message.action}`);
-    if (message.action === 'getMetrics') {
-        console.log(`Sending metrics to content script.`);
-        sendResponse(lastMetrics);
+// Listener for messages from content.js
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if ((message.action === 'displayPopupForSparseContent' || message.action === 'displayPopupForTitle404') && sender.tab) {
+        chrome.tabs.sendMessage(sender.tab.id, { action: 'displayPopup' });
     }
 });
 
-
-function getCurrentTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
-}
-
-// Function to calculate page load time
-function calculatePageLoadTime() {
-    console.log(`Calculating page load time.`);
-    if (pageLoadStartTime) {
-        const pageLoadEndTime = performance.now();
-        return pageLoadEndTime - pageLoadStartTime;
-    } else {
-        return 'Page load time not available';
-    }
-}
 
 // Function to check HTTP status code
 async function checkHttpStatus(url, callback) {
@@ -200,72 +63,20 @@ async function checkHttpStatus(url, callback) {
     }
 }
 
-// Function to get DNS Response Code
-async function getDNSResponse(domain) {
-    console.log(`Fetching DNS response for domain: ${domain}`);
-    
-    // Extract hostname from URL
-    let hostname;
-    try {
-        const url = new URL(domain);
-        hostname = url.hostname;
-    } catch (error) {
-        console.error('Invalid URL:', domain);
-        return 'Invalid URL';
-    }
+function isContentSparse() {
+    const bodyText = document.body.innerText || "";
+    const minTextLength = 100; // define a threshold for text length
+    const minElementCount = 10; // define a threshold for the number of elements
+    const elementCount = document.body.getElementsByTagName('*').length;
 
-    // Google's DNS resolution API endpoint
-    const googleDnsApi = `https://dns.google/resolve?name=${hostname}&type=A`;
-
-    try {
-        const response = await fetch(googleDnsApi);
-        const data = await response.json();
-        if (data.Status === 0 && data.Answer.length > 0) {
-            // Status 0 means NOERROR, the DNS query completed successfully
-            console.log(`DNS response received for ${hostname}:`, data);
-            return 'DNS resolution successful';
-        } else {
-            // Other statuses indicate some sort of error in DNS resolution
-            console.log(`DNS resolution error for ${hostname}:`, data);
-            return 'DNS resolution failed';
-        }
-    } catch (error) {
-        console.error('Error fetching DNS response:', error);
-        return 'Error fetching DNS response';
-    }
+    return bodyText.length < minTextLength && elementCount < minElementCount;
 }
 
-//taken  to the new location predicted by FABLE
-
-
-//soft 404
-
-// Create an array to store URLs that lead to potential soft 404s
-// background.js
-
-// Create an array to store URLs that lead to potential soft 404s
-const soft404Urls = [];
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(`Message received for action: ${message.action}`);
-    if (message.action === 'soft404') {
-        console.log(`Potential soft 404 detected for url: ${message.url}`);
-        soft404Urls.push(message.url);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'checkForSparseContent') {
+        console.error('cghecking for sparse content');
+        if (isContentSparse()) {
+            chrome.runtime.sendMessage({ action: 'displayPopup', reason: 'Sparse content detected' });
+        }
     }
 });
-
-// background.js
-
-// ... (previously defined soft404Urls and message listener)
-
-// Listen for requests for soft 404 URLs from the popup
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(`Message received for action: ${message.action}`);
-    if (message.action === 'getSoft404Urls') {
-        console.log(`Sending soft 404 URLs to the sender.`);
-        sendResponse(soft404Urls);
-    }
-});
-
-
-
--- content.js
+++ content.js
@@ -1,228 +1,92 @@
-// Calculate the TTFB (Time To First Byte)
-function getTTFB() {
-    const performanceTiming = window.performance.timing;
-    return performanceTiming.responseStart - performanceTiming.navigationStart;
-}
-
-// Send the TTFB value to the background script
-const ttfb = getTTFB();
-chrome.runtime.sendMessage({ action: 'ttfb', value: ttfb });
-
 // Listener for messages from the background script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === 'showPopup') {
-        const statusCode = message.statusCode;
-        const currentTime = getCurrentTime();
-        // const dnsResponseCode = message.dnsResponseCode;
-        const pageLoadTime = message.pageLoadTime;
-        const ttfb = message.ttfb;  // TTFB data received from the message
-        const errorType = message.errorType;  // Error type received from the message
-        const errorDescription = message.errorDescription;  // Error description received from the message
-        displayPopup(statusCode, currentTime, pageLoadTime, ttfb, errorType, errorDescription);
-    }
-});
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
-// Returns the current time in HH:MM:SS format
-function getCurrentTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
-}
-
-
-// Displays the banner with various performance metrics and error information
-function displayPopup(statusCode, currentTime, pageLoadTime, ttfb, errorType, errorDescription) {
-    const bannerDiv = document.createElement('div');
-
-    bannerDiv.innerHTML = `
-        <div style="display: flex; align-items: center; height: 35px;">
-            <span style="font-size: 36px;">⚠︎</span>
-            <span style="margin-left: 10px;"><b>Sorry, this link is broken.</b> Do you want to check for a fixed link using FABLE?</span>
-            <div style="flex-grow: 1;"></div> <!-- Spacer to push buttons to the right -->
-            <div style="display: flex; align-items: center;">
-                <a href="https://www.google.com" style="background-color: #4285F4; color: white; padding: 8px 15px; text-decoration: none; border-radius: 5px; outline: none; margin-right: 5px;">Go to fixed link</a>
-                <span id="closeBanner" style="cursor: pointer; margin-left: 5px;">✕</span>
-            </div>
-        </div>`;
-
-    // Apply styles to the banner
-    bannerDiv.style.position = 'fixed';
-    bannerDiv.style.zIndex = 9999;
-    bannerDiv.style.top = '0';
-    bannerDiv.style.left = '0';
-    bannerDiv.style.right = '0';
-    bannerDiv.style.backgroundColor = '#F9F9F9';
-    bannerDiv.style.borderBottom = '1px solid #ccc';
-    bannerDiv.style.padding = '10px';
-    bannerDiv.style.fontSize = '17px';
-    bannerDiv.style.color = 'black';
-    bannerDiv.style.overflow = 'hidden';
-
-    document.body.insertBefore(bannerDiv, document.body.firstChild);
-
-    // After adding to the document, set the margin-top of the body to push content downwards.
-    document.body.style.marginTop = `${bannerDiv.offsetHeight}px`;
-
-    // Add an event listener to the "X" button to remove the banner
-    document.getElementById('closeBanner').addEventListener('click', function () {
-        bannerDiv.remove();
-        document.body.style.marginTop = '0';
-    });
-}
-
-// soft 404
-
-// Function to check if the URL redirects to a different location
-async function checkRedirects() {
-    const currentUrl = window.location.href;
-
-    // Fetch the final URL after following redirects
-    const finalUrl = await getFinalUrl(currentUrl);
-    
-    console.log('Final URL:', finalUrl);
-
-    // If the final URL is different from the current URL, then a redirect has occurred
-    if (finalUrl !== currentUrl) {
-        console.log('Redirect detected. Current URL:', currentUrl, 'Final URL:', finalUrl);
-        // Send a message to the background script indicating a redirect has been detected
-        chrome.runtime.sendMessage({ action: 'redirectDetected', currentUrl: currentUrl, finalUrl: finalUrl });
-    } else {
-        console.log('No redirect detected for URL:', currentUrl);
-    }
-}
-
-// Ensure you call this function in the appropriate context where it makes sense in your extension workflow
-
-// do not issue duplicate requests for page we are looking at - getFinalUrl
-
-//show demo with test cases:
-// create webpage that redirects a b c-404
-// have broken image on webpage that 404s but the original webpage request works fine
-
-//adversarial test cases
-
-
-
-//timeouts:
-//put random ip address in the hostname
-
-//tls connection setup errors: certificate not valid
-
-
-
-
-// Function to fetch the final URL after following redirects
-async function getFinalUrl(url) {
-    try {
-        const response = await fetch(url, {
-            method: 'HEAD',
-            redirect: 'follow'  // Now fetch will follow redirects
-        });
-        // The final URL is available in the response.url
-        return response.url;
-    } catch (error) {
-        console.error('Error following redirects:', error);
-        return url; // In case of an error, return the original URL
-    }
-}
-
-
-// Call the checkRedirects function
-checkRedirects();
+//     if (message.action === 'displayPopup') {
+//         displayPopup();
+//     } else {
+//         console.log("got a generic message");
+//     }
+// });
 
 // content.js
 
-// Function to check if the current page is an error page
-// What if I only look at table title?
-function checkForErrorPage() {
-    const errorIndicators = [
-        "404",
-        "error 404",
-        "404 not found",
-        "error",
-        "page not found",
-        "we can't find the page",
-        "this page isn’t available",
-        "oops",
-        "the page you are looking for can't be found",
-        "something went wrong",
-        "we're sorry, but something went wrong",
-        "the page you requested does not exist",
-        "this page has gone missing",
-        "you seem to be trying to find his way home",
-        "this is somewhat embarrassing, isn’t it?",
-        "it looks like nothing was found at this location",
-        "the requested URL was not found on this server",
-        "the page you are looking for does not exist",
-        "the page you are looking for seems to be missing",
-        "this page is missing",
-        "looks like you have taken a wrong turn",
-        "dead link",
-        "the link you clicked may be broken",
-        "unable to find the page",
-        "we have encountered a problem",
-        "the resource you are looking for might have been removed",
-        "had its name changed",
-        "is temporarily unavailable",
-        "please try again later",
-        "error code: 404",
-        "HTTP 404",
-        "404 error",
-        "404 page",
-        "error page",
-        "invalid URL",
-        "file not found",
-        "broken link",
-        "this page doesn’t exist",
-        "address not found",
-        "this link isn’t working",
-        "no page here",
-        "why am I here?",
-        "404. That’s an error.",
-        "the requested resource is not available",
-        "there's nothing here",
-        "nothing to see here",
-        "page does not exist",
-        "gone",
-        "this URL is invalid",
-        "404’d",
-        "404. Page not found",
-        "404. This is not the web page you are looking for",
-        "we couldn’t find that page",
-        "404. Oops!",
-        "404 page not exist",
-        "404 not found error"
-    ];
+// content.js
 
-    const pageContent = document.body.textContent || document.body.innerText;
-    
-    // Find and log the matched indicator
-    const matchedIndicator = errorIndicators.find(indicator =>
-        pageContent.toLowerCase().includes(indicator.toLowerCase())
-    );
-
-    if (matchedIndicator) {
-        console.log(`Soft 404 detected via page contents: "${matchedIndicator}"`);
-        chrome.runtime.sendMessage({ action: 'soft404', url: window.location.href, message: matchedIndicator });
-    } else {
-        console.log('No soft 404 detected.');
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'checkTitleFor404') {
+        const pageTitle = document.title;
+        if (pageTitle.includes('404')) {
+            chrome.runtime.sendMessage({ action: 'displayPopupForTitle404' });
+        }
     }
+    if (message.action === 'checkForSparseContent') {
+        if (isContentSparse()) {
+            chrome.runtime.sendMessage({ action: 'displayPopupForSparseContent' });
+        }
+    } else if (message.action === 'checkTitleFor404') {
+        const pageTitle = document.title;
+        if (pageTitle.includes('404')) {
+            chrome.runtime.sendMessage({ action: 'displayPopupForTitle404' });
+        }
+    }
+
+    if (message.action === 'displayPopup') {
+        displayPopup();
+    }
+});
+
+function isContentSparse() {
+    const bodyText = document.body.innerText || "";
+    const minTextLength = 100; // define a threshold for text length
+    const minElementCount = 10; // define a threshold for the number of elements
+    const elementCount = document.body.getElementsByTagName('*').length;
+
+    return bodyText.length < minTextLength && elementCount < minElementCount;
 }
 
 
-// Call the checkForErrorPage function
-checkForErrorPage();
+function displayPopup() {
+    console.log("Content Script: Displaying popup");
+  
+    const bannerDiv = document.createElement('div');
+    bannerDiv.id = "BANNERID";
 
+    bannerDiv.innerHTML = `
+    <div style="display: flex; align-items: center; height: 35px;">
+        <span style="font-size: 36px;">⚠︎</span>
+        <span style="margin-left: 10px;"><b>Sorry, this link is broken.</b> Do you want to check for a fixed link using FABLE?</span>
+        <div style="flex-grow: 1;"></div> <!-- Spacer to push buttons to the right -->
+        <div style="display: flex; align-items: center;">
+            <a href="https://www.google.com" style="background-color: #4285F4; color: white; padding: 8px 15px; text-decoration: none; border-radius: 5px; outline: none; margin-right: 5px;">Go to fixed link</a>
+            <span id="closeBanner" style="cursor: pointer; margin-left: 5px;">✕</span>
+        </div>
+    </div>`;
 
+// Apply styles to the banner
+bannerDiv.style.position = 'fixed';
+bannerDiv.style.zIndex = 9999;
+bannerDiv.style.top = '0';
+bannerDiv.style.left = '0';
+bannerDiv.style.right = '0';
+bannerDiv.style.backgroundColor = '#F9F9F9';
+bannerDiv.style.borderBottom = '1px solid #ccc';
+bannerDiv.style.padding = '10px';
+bannerDiv.style.fontSize = '17px';
+bannerDiv.style.color = 'black';
+bannerDiv.style.overflow = 'hidden';
 
+if (document.body) {
+    document.body.insertBefore(bannerDiv, document.body.firstChild);
+  } else {
+    const observer = new MutationObserver(() => {
+      if (document.body) {
+        observer.disconnect();
+        document.body.insertBefore(bannerDiv, document.body.firstChild);
+      }
+    });
 
+    observer.observe(document.documentElement, { childList: true });
+  }
+}
 
-
-
-
-
-
-
-
+// displayPopup();
